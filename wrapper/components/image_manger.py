720be18cd35ffb9e05a1375827f22464acdd0c88
root <panda.subhasish007@gmail.com>
2017-07-28 18:10:37 +0530

    Added uploading Feature

--- wrapper/components/image_manger.py
+++ wrapper/components/image_manger.py
@@ -1,18 +1,18 @@
 from flask_restful import Resource
 from flask import request
-
+from docker import APIClient
+from modules.tls import TLS
 
 class ImageBuilder(Resource):
     def get(self):
         return "Wrong Method, Use POST instead"
 
-    def post(self):
-        requestJson = request.files
-        file = open('/home/subhasishp/Dockerfile','w+')
-        print type(requestJson)
-        #for lines in requestJson:
-        #    file.write(lines)
-        file.close()
-        #content = requestJson['image']
-        print requestJson
+    def put(self):
+        file = request.files['dockerfile']
+        file.save('Dockerfile')
+        tls = TLS()
+        tlsConfig = tls.getTLSconfig('master')
+        cli = APIClient(base_url="tcp://192.168.99.101:2376",tls=tlsConfig)
+        for line in cli.build(path="/home/subhasishp/IOT/iotdatapipeline/",dockerfile="Dockerfile",stream=True,rm=True,tag="latest"):
+            print line
         return "ok"
